903b1dbf6c9a040dfe63a82ac332601cce84dd61
spriyankagirish <spriyankagirish@gmail.com>
2024-12-02 21:19:08 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages,setup
+
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='priyanka',
+    author_email='spriyankagirish@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+
+)
